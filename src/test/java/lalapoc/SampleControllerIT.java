472fa1a1be259541cf1dbc731e43dd86fdf4cc60
Tobias Schacht <tobias.schacht@coremedia.com>
2015-04-23 14:40:35 +0200

    add controller tests

---
+++ src/test/java/lalapoc/SampleControllerIT.java
@@ -0,0 +1,57 @@
+package lalapoc;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LalapocApplication.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port=0"})
+public class SampleControllerIT {
+
+	@Value("${local.server.port}")
+	private int port;
+
+	private URL base;
+	private RestTemplate template;
+
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/");
+		template = new TestRestTemplate();
+	}
+
+	@Test
+	public void getHome() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+		assertThat(response.getBody(), containsString("hello form templates/home.ftl"));
+	}
+
+	@Test
+	public void testReadSampleNodes() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(base.toString() + "samples", String.class);
+		assertThat(response.getBody().matches("(.*pipapo.*){2,}"), is(true));
+	}
+
+	@Test
+	public void testRreateSampleNode() throws Exception {
+		ResponseEntity<String> response = template.postForEntity(base.toString() + "samples", null, String.class);
+		assertThat(response.getBody().matches("(.*pipapo.*){1}"), is(true));
+	}
+
+}
---
+++ src/test/java/lalapoc/SampleControllerTest.java
@@ -0,0 +1,71 @@
+package lalapoc;
+
+import lalapoc.controller.SampleController;
+import lalapoc.entity.SampleNode;
+import lalapoc.repository.SampleNodeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.neo4j.conversion.QueryResultBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class SampleControllerTest {
+
+	private MockMvc mvc;
+
+	@Mock
+	private SampleNodeRepository sampleNodeRepositoryMock;
+
+	@InjectMocks
+	private SampleController testling;
+
+	@Before
+	public void setUp() throws Exception {
+		SampleNode n1 = new SampleNode();
+		SampleNode n2 = new SampleNode();
+
+		n1.setName("N1");
+		n2.setName("N2");
+
+		n1.setId(1L);
+		n2.setId(2L);
+
+		when(sampleNodeRepositoryMock.findAll()).thenReturn(QueryResultBuilder.from(n1, n2));
+
+		mvc = MockMvcBuilders.standaloneSetup(testling).build();
+	}
+
+	@Test
+	public void testHome() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testReadSampleNodes() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/samples").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testCreateSampleNode() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.post("/samples").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+}
