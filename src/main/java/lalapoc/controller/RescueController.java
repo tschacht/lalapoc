64f3a8d9d897e63377bc13c963922d2fcc6cf861
Tobias Schacht <tobias.schacht@coremedia.com>
2015-04-27 14:43:57 +0200

    remove @RestResource as @RepositoryRestResource is preferred

--- src/main/java/lalapoc/controller/RescueController.java
+++ src/main/java/lalapoc/controller/RescueController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
-//@RestResource
 @RestController
 @EnableAutoConfiguration
 public class RescueController {
--- src/main/java/lalapoc/controller/SampleController.java
+++ src/main/java/lalapoc/controller/SampleController.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
 
-//@RestResource
 @RestController
 @EnableAutoConfiguration
 public class SampleController {
