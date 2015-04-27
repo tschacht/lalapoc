23359907b22fb700362934c98b7220bbf7729ee8
Tobias Schacht <tobias.schacht@coremedia.com>
2015-04-27 11:13:02 +0200

    add a RESTful (following HATEOAS) frontend to SampleNodes under path "rest-samples" using @RepositoryRestResource

--- src/main/java/lalapoc/repository/SampleNodeRepository.java
+++ src/main/java/lalapoc/repository/SampleNodeRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.CypherDslRepository;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Collection;
 
+@RepositoryRestResource(collectionResourceRel = "rel-samples", path = "rest-samples")
 public interface SampleNodeRepository extends GraphRepository<SampleNode>, CypherDslRepository<SampleNode> {
 
 	@Fetch
